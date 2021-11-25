feaa4d19b84842cea485915e7f58788c040fb73f
Philip Heltweg <pheltweg@gmail.com>
2021-11-25 14:32:36 +0100

    Chore: Removed vuex and vue-class-component

---
+++ src/components/Timer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="timer">
+    <div class="timer__minutes">
+      {{ String(Math.floor(seconds / 60)).padStart(2, "0") }}
+    </div>
+    <div class="timer__colon">:</div>
+    <div class="timer__seconds">
+      {{ String(seconds % 60).padStart(2, "0") }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    startSeconds: Number,
+  },
+  data() {
+    return {
+      seconds: this.startSeconds || 1500,
+    };
+  },
+  mounted(): void {
+    setInterval(() => {
+      this.seconds -= 1;
+    }, 1000);
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.timer {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Timer :startSeconds="25 * 60" />
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from "vue";
+import Timer from "../components/Timer.vue";
 
-@Options({
+export default defineComponent({
   components: {
-    HelloWorld,
+    Timer,
   },
-})
-export default class Home extends Vue {}
+});
 </script>
